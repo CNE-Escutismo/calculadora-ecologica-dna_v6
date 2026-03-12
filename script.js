b81ea86d971b99206f4eeaefde557d16839bbb1b
Nuno Faria <130001008+nunofaria13@users.noreply.github.com>
2026-03-12 19:24:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -275,8 +275,8 @@ function showResults() {
     });
   }
 
-  /* ===== Dicas — 1 aleatória por categoria ===== */
-  const labels = ['Deslocações', ['Tipologia de', 'Atividade'], 'Alimentação', 'Água', 'Energia', 'Resíduos'];
+/* ===== Dicas — 1 aleatória por categoria ===== */
+const labels = ['Deslocações', 'Tipologia de Atividade', 'Alimentação', 'Água', 'Energia', 'Resíduos'];
 
 const dicas = {
   'Deslocações': [
@@ -355,4 +355,5 @@ const dicas = {
     tipEl.style.display = 'block';
   }
 }
+
 
